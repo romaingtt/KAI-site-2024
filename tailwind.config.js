1635c10f86ad7c50fd60eb25fc2cdb92c6f516a6
romaingtt <hello@rgtt.fr>
2024-07-21 14:02:21 +0200

    feat: menu + footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 module.exports = {
     content: ["*/**/*.html"],
     theme: {
+        colors: {
+            'black': '#3A3335',
+            'orange': '#FB3640',
+            'blue': '#321EFF',
+            'white': '#ffffff'
+        },
         container: {
             center: true,
         },
         extend: {
-            colors: {},
+            colors: {
+                'whithish': '#FDF0D5',
+                'greenish': '#3AB795'
+            },
         },
     },
     variants: {},
-    plugins: [require("@tailwindcss/typography")],
+    plugins: [
+        require("@tailwindcss/typography")
+    ],
 };
